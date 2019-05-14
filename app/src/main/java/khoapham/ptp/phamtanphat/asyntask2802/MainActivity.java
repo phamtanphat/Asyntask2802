586112c1d58ca0badf4f95af1067805b21663885
Phạm Tấn Phát <phatdroid94@gmail.com>
2019-05-14 11:43:01 +0700

    a

--- app/src/main/java/khoapham/ptp/phamtanphat/asyntask2802/MainActivity.java
+++ app/src/main/java/khoapham/ptp/phamtanphat/asyntask2802/MainActivity.java
@@ -9,5 +9,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        - Truoc khi bat dau down load :
+//            + Cho hien thanh progressbar
+//        - Trong qua trinh download :
+//            + An thanh progressbar;
+//            + Hien thi text phan tram down load
+//            + 1s => 20 phan tram;
+//        - Down load xog hien thi tam hinh:
+//            + Tat thanh progressbar va text
     }
 }
